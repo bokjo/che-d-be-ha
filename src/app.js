9085bceb1f60257de9804d37d3fdd6a2b46dd348
Bojanche Stojchevski <bojanche@propel.us>
2023-01-25 21:34:19 +0100

    feat: add logger and basic eslint setup

--- src/app.js
+++ src/app.js
@@ -1,21 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const { sequelize } = require("./model");
+const { getProfile } = require("./middleware/getProfile");
+
+const { logger } = require("./logger");
+const { pinoHttp } = require("pino-http");
+
 const app = express();
 app.use(bodyParser.json());
-app.set('sequelize', sequelize)
-app.set('models', sequelize.models)
+app.use(pinoHttp({ logger }));
+app.set("sequelize", sequelize);
+app.set("models", sequelize.models);
 
 /**
  * FIX ME!
  * @returns contract by id
  */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
-    res.json(contract)
-})
+app.get("/contracts/:id", getProfile, async (req, res) => {
+  const { Contract } = req.app.get("models");
+  const { id } = req.params;
+  const contract = await Contract.findOne({ where: { id } });
+  if (!contract) return res.status(404).end();
+  res.json(contract);
+});
+
 module.exports = app;
